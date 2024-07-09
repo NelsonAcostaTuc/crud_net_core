fa9e1245d7fea5000f0f5c473fc206aeac831ce1
Nelson Acosta <nelsongacosta@gmail.com>
2024-07-09 15:16:20 -0300

    first commit

---
+++ Data/appDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using CRUD.Models;
+
+namespace CRUD.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
